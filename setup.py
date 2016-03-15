9e62e86abd7f96e1f777b0099b9d1011b1c0fc54
Min RK <benjaminrk@gmail.com>
2016-03-15 16:47:38 +0100

    dependencies

--- setup.py
+++ setup.py
@@ -40,6 +40,12 @@ setup_args = dict(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
     ],
+    requires=[
+        'ipykernel>=4.3',
+        'jupyter-client',
+        'pyzmq>=15.2',
+        'tornado>=4',
+    ],
 )
 
 if 'bdist_wheel' in sys.argv:
